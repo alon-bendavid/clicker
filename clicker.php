41d7fdedc8bae9c37cac501e8cf539984425c2a7
alon-bendavid <ben-david.alon@laplateforme.io>
2023-03-01 16:06:07 +0100

    before chnaging the logic to speed

--- clicker.php
+++ clicker.php
@@ -3,10 +3,11 @@ include_once("header.php");
 ?>
 
 <body>
+    <h4 id="counter">0</h4>
     <div class="btn_div"></div>
-    <button id="bonus1" value="50">bonus 1</button>
-    <button id="bonus2">bonus 2</button>
-    <button id="bonus3">bonus 3</button>
+    <button id="bonus1" value="1">bonus 1</button>
+    <button id="bonus2" value="2">bonus 2</button>
+    <button id="bonus3" value="3">bonus 3</button>
 
 </body>
 
--- js/clickerScript.js
+++ js/clickerScript.js
@@ -1,11 +1,37 @@
-
+let speed = 1000; // this value set the rate of which the function will be intervald thus will effect the speed of increasment
 let count = 0;
 
-const bonus1 = document.getElementById("bonus1");
-bonus1.addEventListener("click", ()=>{
-count += 1;
-    // console.log(bonus1.value);
-    console.log(count);
+let counter = document.getElementById("counter");
+const bonus = document.querySelectorAll("button");
+
+//event listenr to catch the click of the button and then recalculate of the count
+bonus.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+    let newCount =   raiseBonus (btn.value);
+        //  count += intBtn*10;
+        console.log(newCount);
+    } )
+});
+  
+
+  
+//clicked btn value enter as a permater and raise the count by multiply the value
+function raiseBonus (btnValue){
+    parseInt(btnValue);
+    return count += btnValue*10;
+}
 
 
-} )
+
+//raise the value constenetly by the seconds
+function TimeRaiseCount(){
+//    let time = new Date();
+  
+// seconds = time.getSeconds();
+count += 5;
+counter.innerHTML =  count;
+
+}
+
+setInterval(TimeRaiseCount,1000);
+
