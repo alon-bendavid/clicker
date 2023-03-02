05e092938ecf7266682ae044232f8c602e9cc541
alon-bendavid <ben-david.alon@laplateforme.io>
2023-03-02 21:07:43 +0100

    game logic is working, left to connect the user

--- clicker.php
+++ clicker.php
@@ -3,12 +3,14 @@ include_once("header.php");
 ?>
 
 <body>
-    <h4 id="counter">0</h4>
-    <div class="btn_div"></div>
-    <button id="bonus1">bonus 1</button>
-    <button id="bonus2">bonus 2</button>
-    <button id="bonus3">bonus 3</button>
-
+    <h4 id="message"> </h4>
+    <h2 id="counter">0</h2>
+    <button id="laborClick">Click to gain credit!</button>
+    <div class="btn_div">
+        <button id="bonus1">bonus 1</button>
+        <button id="bonus2">bonus 2</button>
+        <button id="bonus3">bonus 3</button>
+    </div>
 </body>
 
 </html>
--- js/clickerScript.js
+++ js/clickerScript.js
@@ -1,71 +1,111 @@
-let speed = 1000; // this value set the rate of which the function will be intervald thus will effect the speed of increasment
-let count = 0;
+// let speed =  1000; 
 
+let speed = parseInt(localStorage.getItem('speed')) || 1000; 
+console.log(speed);// set the interval refresh rate
+let count = 0;
 let counter = document.getElementById("counter");
-//increasces the speed by precantge
-let value1 = 0.3;
-let value2 = 0.5;
-let value3 = 0.6;
+
+let message = document.getElementById("message");
+
+//prices
+let price1 = 1;//5
+let price2 = 2;//25
+let price3 = 3;//100 
+
+
+//increasces the speed by precantge - bonus advantage
+let speed1 = 0.01;
+let speed2 = 0.015;
+let speed3 = 0.03;
 
 //buttons selcotrs
+const laborClick = document.getElementById("laborClick");
 const bonus1 = document.getElementById("bonus1");
 const bonus2 = document.getElementById("bonus2");
 const bonus3 = document.getElementById("bonus3");
 
-//onclick event listeners
+// laborClick
+laborClick.addEventListener("click",()=>{
+    count += 1;
+counter.innerHTML =  count;
+
+})
+
+//when the person choose a bonus he is being charged and getting speed advantage
+///////bonus 1/////////
 bonus1.addEventListener("click",()=>{
-    clearInterval(interval);
-    speedTime(value1);
-    setInterval(gameLoop,speed)
+    if (count>=price1){
+        message.innerHTML = "You just got yourself an extra- "+speed1*100+"% speed advantge "
+        count = count-price1;
+//         console.log(count);
+clearInterval(interval);
+speedTime(speed1);
+ interval =  setInterval(gameLoop,speed);
+
+localStorage.setItem('speed', speed.toString());
+console.log(speed);
+    }else{
+        message.innerHTML = "Not enough credit!"
+    }
 })
+///////bonus 2/////////
 bonus2.addEventListener("click",()=>{
-    clearInterval(interval);
-    speedTime(value2);
-    setInterval(gameLoop,speed)
+    if (count>=price2){
+        message.innerHTML = "You just got yourself an extra- "+speed2*100+"% speed advantge "
+
+        count = count-price2;
+        console.log(count);
+        clearInterval(interval);
+        speedTime(speed2);
+     interval =    setInterval(gameLoop,speed)
+localStorage.setItem('speed', speed);
+
+    }else{
+        message.innerHTML = "Not enough credit!"
+    }
 })
+///////bonus 3/////////
 bonus3.addEventListener("click",()=>{
-    clearInterval(interval);
-    speedTime(value3);
-    setInterval(gameLoop,speed)
+    if (count>=price3){
+        message.innerHTML = "You just got yourself an extra- "+speed3*100+"% speed advantge "
+
+        count = count-price3;
+        console.log(count);
+        clearInterval(interval);
+        speedTime(speed3);
+       interval =  setInterval(gameLoop,speed)
+localStorage.setItem('speed', speed.toString());
+
+    }else{
+        message.innerHTML = "Not enough credit!"
+    }  
+    
+    
 })
 
 
-//raise the value constenetly by the seconds
+//keep update the value with 1 point increacment
 function gameLoop(){
-//    let time = new Date();
-  
-// seconds = time.getSeconds();
 count += 1;
 counter.innerHTML =  count;
 console.log(speed);
 
 }
-let interval = setInterval(gameLoop,speed);
-// let interval = setInterval(gameLoop,speed);
-
-
+// function speedTime(precenatge){
+//     speed -= precenatge*speed;
+   
+// }
 
 function speedTime(precenatge){
-    speed -= precenatge*speed;
+  return  speed -= precenatge*speed;
    
 }
+let interval;
+//Starting the game
+if(!interval){
+    interval = setInterval(gameLoop,speed);
+    
+}
 
+localStorage.setItem('speed', speed.toString());
 
-//////////////////////////////////////////////////////////////DEAD CODE///////////////////////////////////////
-//event listenr to catch the click of the button and then recalculate of the count
-// bonusBtns.forEach(btn => {
-//     btn.addEventListener("click", ()=>{
-//     let newSpeed =  raiseBonus (btn.value);
-//         //  count += intBtn*10;
-//     return newSpeed;
-//         // console.log(newCount);
-//     } )
-// });
-  
-
-  
-// //clicked btn value enter as a permater and raise the count by multiply the value
-// function raiseBonus (btnValue){
-//     parseInt(btnValue);
-//     console.log(speed -= speed * (btnValue*10)/100); 
-// }
